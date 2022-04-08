f98f6f1ede4a334d3d96199491af1709e5ffcb5d
Erik Högberg <erikh_93@hotmail.com>
2022-04-09 01:21:36 +0200

    rod wip

--- Lame2022/Assets/Scripts/PlayerScript.cs
+++ Lame2022/Assets/Scripts/PlayerScript.cs
@@ -11,11 +11,15 @@ public class PlayerScript : MonoBehaviour
     public SpriteShapeController line;
     public float lineSegmentLength = 10;
 
-    public Rigidbody hook;
+    public Rigidbody2D hook;
+    public Transform Rod;
+    public Transform RodEnd;
+
+    public float RodAngle = 20;
 
     Queue<Vector3> hookHistory;
 
-    float hookRecordTimer = -1f;
+    float hookRecordTimer = 0f;
 
     void Start()
     {
@@ -24,6 +28,16 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
+
+        hookRecordTimer -= Time.deltaTime;
+
+        if (hookRecordTimer < 0)
+        {
+            // if (hookHistory.Count < 1 || Vector3.Distance(hook.position, hookHistory.Peek()) > .01f)
+            hookHistory.Enqueue(hook.position);
+            hookRecordTimer += .2f;
+        }
+
         float x = 0;
         float y = 0;
 
@@ -36,16 +50,17 @@ public class PlayerScript : MonoBehaviour
         if (Keyboard.current.wKey.isPressed)
         {
             y += speed * Time.deltaTime;
+            hook.AddForce(Vector2.up*10);
         }
-        if (Keyboard.current.aKey.wasPressedThisFrame)
+        if (Keyboard.current.aKey.isPressed)
         {
             x -= speed * Time.deltaTime;
         }
-        if (Keyboard.current.sKey.wasPressedThisFrame)
+        if (Keyboard.current.sKey.isPressed)
         {
             y -= speed * Time.deltaTime;
         }
-        if (Keyboard.current.dKey.wasPressedThisFrame)
+        if (Keyboard.current.dKey.isPressed)
         {
             x += speed * Time.deltaTime;
         }
@@ -56,16 +71,26 @@ public class PlayerScript : MonoBehaviour
         transform.position = position;
 
         line.spline.Clear();
-        line.spline.InsertPointAt(0, transform.localPosition);
+        Vector3 lastPos = RodEnd.position;
+        line.spline.InsertPointAt(0, lastPos);
+
+        while (hookHistory.Count > 10)
+        {
+            hookHistory.Dequeue();
+        }
         foreach (var item in hookHistory)
         {
-            line.spline.InsertPointAt(0, transform.InverseTransformPoint(item));
+            if (Vector3.Distance(lastPos, item) > .1f)
+                line.spline.InsertPointAt(0, item);
+
+            lastPos = item;
         }
-        line.spline.InsertPointAt(0, transform.InverseTransformPoint(hook.position));
-       //float randomheight =Random.RandomRange(0.8f, 1.2f);
-       // var scale = transform.localScale;
-       // scale.y = randomheight;
-       // transform.localScale = scale;
+        if (Vector3.Distance(lastPos, hook.position) > .1f)
+            line.spline.InsertPointAt(0, hook.position);
+        //float randomheight =Random.RandomRange(0.8f, 1.2f);
+        // var scale = transform.localScale;
+        // scale.y = randomheight;
+        // transform.localScale = scale;
 
     }
 }
