5a8b80df461c9de78eb1b9770b83c1de95e3af60
LukasLundin <91780009+LukasLundin@users.noreply.github.com>
2022-04-09 22:02:58 +0200

    Menu art and stuff

--- Lame2022/Assets/Scripts/menuStart.cs
+++ Lame2022/Assets/Scripts/menuStart.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menuStart : MonoBehaviour
 {
 	public GameObject startMenu;
-    // Start is called before the first frame update
-    void Start()
+	public GameObject gameOverMenu;
+
+	// Start is called before the first frame update
+	void Start()
     {		
 		startMenu.SetActive(true);
     }
+
+	public void RestartGame() 
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+	}
+
+	public void GameOverScreen() 
+	{
+		gameOverMenu.SetActive(true);
+	}
 }
