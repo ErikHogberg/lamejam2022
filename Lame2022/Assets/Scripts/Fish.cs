da933aff138c91e6b5240c9d046c08849d5e6734
Erik Högberg <erikh_93@hotmail.com>
2022-04-09 11:57:02 +0200

    line wip

--- Lame2022/Assets/Scripts/Fish.cs
+++ Lame2022/Assets/Scripts/Fish.cs
@@ -2,23 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fish : MonoBehaviour
+public class Fish : MonoBehaviour {
 
-{
+	public Rigidbody2D rb;
+	public Vector2 speed;
 
-    public Rigidbody2D rigidbody2D;
-    public Vector2 speed;
+	// Start is called before the first frame update
+	void Start() {
+		rb.velocity = speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody2D.velocity =speed;
-        
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update() {
+
+	}
 }
--- Lame2022/Assets/Scripts/PlayerScript.cs
+++ Lame2022/Assets/Scripts/PlayerScript.cs
@@ -4,93 +4,106 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.U2D;
 
-public class PlayerScript : MonoBehaviour
-{
-    public float speed = 1;
-    public Transform mousebox;
-    public SpriteShapeController line;
-    public float lineSegmentLength = 10;
+public class PlayerScript : MonoBehaviour {
+	public float speed = 1;
+	public Transform mousebox;
+	public SpriteShapeController line;
+	public float lineSegmentLength = 10;
 
-    public Rigidbody2D hook;
-    public Transform Rod;
-    public Transform RodEnd;
+	public Rigidbody2D hook;
+	public Transform Rod;
+	public Transform RodEnd;
 
-    public float RodAngle = 20;
 
-    Queue<Vector3> hookHistory;
+	[Range(0, 90)]
+	public float RodMaxAngle = 20;
+	public float LinePointDistance = .5f;
+	public int LinePointCount= 20;
+	public float LineFloatRate = 1;
+	public float LineWhipRate = 1;
 
-    float hookRecordTimer = 0f;
 
-    void Start()
-    {
-        hookHistory = new Queue<Vector3>(10);
-    }
+	List<Vector3> hookPoints = new List<Vector3>();
+	// Vector3[] hookPoints = new Vector3[]{
+	// 	Vector3.up * 1,
+	// 	Vector3.up * 2,
+	// 	Vector3.up * 3,
+	// 	Vector3.up * 4,
+	// 	Vector3.up * 5,
+	// };
 
-    void Update()
-    {
+	void Start() {
+		for (int i = 0; i < LinePointCount; i++) {
+			hookPoints.Add(Vector3.up * 1 * .2f);
+		}
+	}
 
-        hookRecordTimer -= Time.deltaTime;
+	void Update() {
 
-        if (hookRecordTimer < 0)
-        {
-            // if (hookHistory.Count < 1 || Vector3.Distance(hook.position, hookHistory.Peek()) > .01f)
-            hookHistory.Enqueue(hook.position);
-            hookRecordTimer += .2f;
-        }
 
-        float x = 0;
-        float y = 0;
 
-        Vector2 mouseposition = Pointer.current.position.ReadValue();
-        var mouseboxposition = new Vector3(mouseposition.x, mouseposition.y, 0);
-        mouseboxposition = Camera.main.ScreenToWorldPoint(mouseboxposition);
-        mouseboxposition.z = 0;
-        mousebox.position = mouseboxposition;
+		float x = 0;
+		float y = 0;
 
-        if (Keyboard.current.wKey.isPressed)
-        {
-            y += speed * Time.deltaTime;
-            hook.AddForce(Vector2.up*10);
-        }
-        if (Keyboard.current.aKey.isPressed)
-        {
-            x -= speed * Time.deltaTime;
-        }
-        if (Keyboard.current.sKey.isPressed)
-        {
-            y -= speed * Time.deltaTime;
-        }
-        if (Keyboard.current.dKey.isPressed)
-        {
-            x += speed * Time.deltaTime;
-        }
+		Vector2 mouseposition = Pointer.current.position.ReadValue();
+		var mouseboxposition = new Vector3(mouseposition.x, mouseposition.y, 0);
+		mouseboxposition = Camera.main.ScreenToWorldPoint(mouseboxposition);
+		mouseboxposition.z = 0;
+		mousebox.position = mouseboxposition;
 
-        var position = transform.position;
-        position.x += x;
-        position.y += y;
-        transform.position = position;
+		float rodNewAngle = Vector3.SignedAngle(Vector3.up, mouseboxposition - Rod.position, Vector3.back);
+		rodNewAngle = Mathf.Sign(rodNewAngle) * Mathf.Min(Mathf.Abs(rodNewAngle), RodMaxAngle);
 
-        line.spline.Clear();
-        Vector3 lastPos = RodEnd.position;
-        line.spline.InsertPointAt(0, lastPos);
+		Rod.rotation = Quaternion.AngleAxis(rodNewAngle, Vector3.back);
 
-        while (hookHistory.Count > 10)
-        {
-            hookHistory.Dequeue();
-        }
-        foreach (var item in hookHistory)
-        {
-            if (Vector3.Distance(lastPos, item) > .1f)
-                line.spline.InsertPointAt(0, item);
+		hookPoints[0] = RodEnd.position;
+		for (int i = 1; i < hookPoints.Count; i++) {
 
-            lastPos = item;
-        }
-        if (Vector3.Distance(lastPos, hook.position) > .1f)
-            line.spline.InsertPointAt(0, hook.position);
-        //float randomheight =Random.RandomRange(0.8f, 1.2f);
-        // var scale = transform.localScale;
-        // scale.y = randomheight;
-        // transform.localScale = scale;
+			hookPoints[i] += Vector3.up * LineFloatRate * Time.deltaTime;
 
-    }
+			Vector3 prevPos = hookPoints[i - 1];
+			Vector3 oldPos = hookPoints[i];
+			if (Vector3.Distance(oldPos, prevPos) > LinePointDistance) {
+				hookPoints[i] = prevPos + (oldPos - prevPos).normalized * LinePointDistance;
+			}
+		}
+		hook.MovePosition(hookPoints[hookPoints.Count-1]);
+
+		if (Keyboard.current.wKey.isPressed) {
+			y += speed * Time.deltaTime;
+			hook.AddForce(Vector2.up * 10);
+		}
+		if (Keyboard.current.aKey.isPressed) {
+			x -= speed * Time.deltaTime;
+		}
+		if (Keyboard.current.sKey.isPressed) {
+			y -= speed * Time.deltaTime;
+		}
+		if (Keyboard.current.dKey.isPressed) {
+			x += speed * Time.deltaTime;
+		}
+
+		var position = transform.position;
+		position.x += x;
+		position.y += y;
+		transform.position = position;
+
+		line.spline.Clear();
+		Vector3 lastPos = RodEnd.position;
+		line.spline.InsertPointAt(0, lastPos);
+
+		foreach (var item in hookPoints) {
+			if (Vector3.Distance(lastPos, item) > .1f)
+				line.spline.InsertPointAt(0, item);
+
+			lastPos = item;
+		}
+		if (Vector3.Distance(lastPos, hook.position) > .1f)
+			line.spline.InsertPointAt(0, hook.position);
+		//float randomheight =Random.RandomRange(0.8f, 1.2f);
+		// var scale = transform.localScale;
+		// scale.y = randomheight;
+		// transform.localScale = scale;
+
+	}
 }
