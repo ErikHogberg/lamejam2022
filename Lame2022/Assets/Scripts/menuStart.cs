b92d1cd7cb4e96c15566bcd054f8402a583615b0
LukasLundin <91780009+LukasLundin@users.noreply.github.com>
2022-04-09 18:40:50 +0200

    Menus

---
+++ Lame2022/Assets/Scripts/menuStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuStart : MonoBehaviour
+{
+	public GameObject startMenu;
+    // Start is called before the first frame update
+    void Start()
+    {		
+		startMenu.SetActive(true);
+    }
+}
